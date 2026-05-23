cfc9ae7394851a922fd3a5ddcceba39a0044bd78
gentlebazzr-lang <gentlebazzr@gmail.com>
2026-05-23 14:20:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 import xml.etree.ElementTree as ET
+import json
+from datetime import datetime
+import time
 
 def fetch_economic_data():
     url = "https://nfs.faireconomy.media/ff_calendar_thisweek.xml"
@@ -39,12 +42,37 @@ def fetch_economic_data():
             })
 
     print("--- Forex Factory Filtered Data---")
+    pine_timestamps = []
+    
     if not filtered_events:
         print("No High Impact News or Holidays for the selected currencies for this week.")
     else:
         for item in filtered_events:
-            print(f"[{item['date']} {item['time']}] {item['currency']} ({item['impact']}): {item['title']}")
+            print(f"[{item['date']} {item['time']}] {item['currency']} ({item['impact']}): {item['title']}") # for readable format
+
+    
+            try:
+                full_time_str = f"{item['date']} {item['time']}"
+                dt = datetime.strptime(full_time_str, "%m-%d-%Y %I: %M%p")
+                unix_ms = int(time.mktime(dt.timetuple())) * 1000
+                pine_timestamps.append(unix_ms)
+            except Exception as e:
+                continue 
+                
+    output_payload = {
+        "last_updated": datetime.utcnow().strftime("%Y-%m_%d %H: %M: %S UTC"),
+        "high_impact_timestamps": pine_stamps
+    }
+    with open("news_data.json", "w") as json_file:
+        json.dump(output_payload, json_file, indent=4)
+    print ("successfully generated news_data.json for public distribution")
+
+            
+                                                                                       
 
 if __name__ == "__main__":
     fetch_economic_data ()
+
+                                                 
+                                                                                   
     
